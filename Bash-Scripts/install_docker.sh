b3afb5919230fe4cbde45b42f29e6db24f20175b
Can Ta <can.ta@corp.virtualtanet.com>
2019-11-30 16:37:24 +0100

    .

--- Bash-Scripts/install_docker.sh
+++ Bash-Scripts/install_docker.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#!pdxevBwx%c9
 
 # Official way to install latest version: get.docker.com
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
 #Add user to group to use docker as a non-root user
-usermod -aG docker virtual
+usermod -aG docker $USER
 
 # Install Docker Compose github.com/docker/compose/release
 curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
@@ -16,10 +15,3 @@ chmod +x /usr/local/bin/docker-compose
 curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
     chmod +x /tmp/docker-machine &&
     sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
-
-#
-#
-
-
-W2K2019-TPL-CORE
-W2K2019-TPL-GUI
---
+++ Bash-Scripts/install_treafik.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Run: install_docker.sh first
+
+
