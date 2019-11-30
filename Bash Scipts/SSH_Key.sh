23a28cb2da29f77279911636d4b73187c9506319
Can Ta <can.ta@corp.virtualtanet.com>
2019-11-30 15:12:58 +0100

    .

--- Bash Scipts/SSH_Key.sh
+++ Bash Scipts/SSH_Key.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Create SSH Key
+ssh-keygen -t ed25519
+
+# Create ~/.ssh/authorized_keys
+
+
+
+# Copy ~/.ssh/id_ed25519.pub from your client to the ~/.ssh/authorized_keys file on your server.
+
+
+
 # .ssh/authorized_keys file needs to contain the public key
 # Then you can use the private key to login
 #
